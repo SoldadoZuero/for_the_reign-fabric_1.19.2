8eaffa59483c7b38cee8690b7af73497943f15ab
SoldadoZuero <skeleton_fake@outlook.com>
2024-01-05 16:09:13 -0300

    Initial commit

---
+++ src/client/java/net/soldadozuero/ForTheReignClient.java
@@ -0,0 +1,10 @@
+package net.soldadozuero;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForTheReignClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
