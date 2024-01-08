a567b381599944faf92e2c5a39122d710fb564c5
SoldadoZuero <skeleton_fake@outlook.com>
2024-01-08 15:15:27 -0300

    Started the project

---
+++ src/main/java/net/soldadozuero/forthereign/ForTheReignClient.java
@@ -0,0 +1,10 @@
+package net.soldadozuero.forthereign;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForTheReignClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
